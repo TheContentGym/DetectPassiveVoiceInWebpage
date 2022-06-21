881e4e143530f070ac0f29b3a277c24061834daf
gtalwargit config --global user.name gtalwargit config --global user.name gtalwar <gyanesh.talwar@gmail.com>
2022-06-21 10:39:32 +0530

    Update DetectPassiveVoice.py

--- DetectPassiveVoice.py
+++ DetectPassiveVoice.py
@@ -3,16 +3,22 @@ import requests
 import lxml
 import spacy
 nlp = spacy.load("en_core_web_sm")
-
-url = 'https://thecontentgym.wordpress.com/2022/05/09/a-non-engineer-attending-a-marathon-5-full-days-python-training-with-experienced-programmers/'
+#1) Send a get query to the website and receive HTML
+url = 'https://docs.up42.com/data'
 response = requests.get(url)
+#2) Parse the HTML
 soup = BeautifulSoup(response.text,'lxml')
+#3) Extracting the data we actually want.
 bd = soup.find('body')
-
+#4) Check the data in the variable for passive voice using the SpaCy library
 doc = nlp(bd.text)
 for token in doc:
     if (token.dep_ == "nsubjpass"):
-        print("passive voice found. Nominal subject:",token.text)
+#5) Print the results        
+        print("passive voice found. Nominal subject:",token.text,token.lemma_, token.pos_, token.tag_, token.dep_)
     elif (token.dep_ == "auxpass"):
-        print("passive voice found in nominal subject:",token.text)
+        print("passive voice found in nominal subject:",token.text,token.lemma_, token.pos_, token.tag_, token.dep_)
 
+    else:
+        print('passive voice not detected')
+        break 
